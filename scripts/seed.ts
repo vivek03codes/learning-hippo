a10ae34cc50d6b7a50c07b3ccb1fd9e1c3789929
Vivek Yadav <g21cs.vivek.yadav@gnkhalsa.edu.in>
2024-03-03 19:55:42 +0530

    Deloyment

---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require( "@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+    try {
+        await database.category.createMany({
+            data: [
+                {name: "Computer Science"},
+                {name: "Music"},
+                {name: "Photography"},
+                {name: "Filming"},
+                {name: "Fitness"},
+                {name: "Accounting"},
+                {name: "Engineering"},
+            ]
+        });
+        console.log("Success");
+    } catch (error) {
+        console.log("Error seeding the database categories", error)
+    }finally{
+        await database.$disconnect();
+    }
+}
+
+main();
